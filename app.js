127d4d0c43464db664b79aad314ec7c155855239
maximora06 <maximohernan123@gmail.com>
2023-07-25 20:07:57 -0300

    holas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 app.use(express.static('public'))
 
-app.listen(3000,()=>console.log('servidor 3000 functionando'))
+app.listen(process.env.PORT || 3000, function(){console.log('servidor 3000 functionando')})
 
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'))
